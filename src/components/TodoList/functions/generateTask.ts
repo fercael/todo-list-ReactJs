ba22b8ca335ee76e448a24b3138939745574d377
carlos fernandes <fercael@outlook.com>
2023-01-31 13:22:35 -0300

    Modify funcao generateTask

--- src/components/TodoList/functions/generateTask.ts
+++ src/components/TodoList/functions/generateTask.ts
@@ -3,7 +3,7 @@ import { ITask } from "../interfaces/task"
 
 export const generateTask = (list : Array<ITask>, text: string): {} => {
   return {
-    id: list.length,
+    id: list.length + 1,
     text: text,
     status: false
   }
